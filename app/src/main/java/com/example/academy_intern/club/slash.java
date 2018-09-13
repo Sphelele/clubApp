2b9b39edd0d4ea5a2136fbeefb1d71070ebad929
Sphelele Myeni <siphelele@thedigitalacademy.co.za>
2018-09-13 08:08:35 +0200

    first commit

---
+++ app/src/main/java/com/example/academy_intern/club/slash.java
@@ -0,0 +1,13 @@
+package com.example.academy_intern.club;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class slash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slash);
+    }
+}
